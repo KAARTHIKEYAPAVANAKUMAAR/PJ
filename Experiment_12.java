7571042f30220317fb5c71336f143bda43f03494
KAARTHIKEYAPAVANAKUMAAR <54408283+KAARTHIKEYAPAVANAKUMAAR@users.noreply.github.com>
2021-02-10 11:45:08 +0530

    Create Experiment_12.java

---
+++ Experiment_12.java
@@ -0,0 +1,18 @@
+imprt java.util.*;
+publiuc class ExceptionHandling{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		try{
+			System.out.print("Enter any two values");
+			int a=sc nextInt();
+			int b= sc.nextInt();
+			int c=a/b;
+		}
+		catch(ArithmaticExcxeption ae){
+			System.out.print("Arithmatic Exception occured");
+		}
+		catch(InputMismatchException ie){
+			System.out,print("Input mismatch exception occured");
+		}
+	}
+}
