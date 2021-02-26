7a1e48224eb52ac890a09461976af6a292bbac5f
KAARTHIKEYAPAVANAKUMAAR <54408283+KAARTHIKEYAPAVANAKUMAAR@users.noreply.github.com>
2021-02-26 20:54:53 +0530

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,25 @@
+package javalab;
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+public class Tree {
+	//JFrame j;
+	Tree(){
+		DefaultMutableTreeNode Mycomputer=new DefaultMutableTreeNode("My computer");
+		DefaultMutableTreeNode cDrive=new DefaultMutableTreeNode("c Drive");
+		DefaultMutableTreeNode dDrive=new DefaultMutableTreeNode("d Drive");
+		DefaultMutableTreeNode Images=new DefaultMutableTreeNode("images");
+		Mycomputer.add(cDrive);
+		Mycomputer.add(dDrive);
+		dDrive.add(Images);
+		JTree tree=new JTree(Mycomputer);
+		JFrame j=new JFrame(); 
+		j.add(tree); 
+		j.setSize(500,500); 
+		j.setVisible(true); 
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	}
+	public static void main(String[] args) {
+		new Tree();
+	}
+
+}
