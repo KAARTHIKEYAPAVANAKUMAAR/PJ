b9ec9c8792bd7a7b6fa9042f9d9b7bb5c2623679
KAARTHIKEYAPAVANAKUMAAR <54408283+KAARTHIKEYAPAVANAKUMAAR@users.noreply.github.com>
2021-02-26 21:08:26 +0530

    Create TableShort.java

---
+++ TableShort.java
@@ -0,0 +1,19 @@
+package javalab;
+import java.awt.*; 
+import javax.swing.*; 
+public class TableShort extends JFrame{ 
+	public TableShort(){ 
+		String [] columnNames = { "Name","RollNo"}; 
+		Object [][] data = {{"Kaarthikeya","3"},{"Pavana","7"},{"Kumaar","11"}}; 
+		JTable table = new JTable(data, columnNames); 
+		JScrollPane scrollPane = new JScrollPane(table); 
+		add(scrollPane); 
+	} 
+	public static void main(String args[]){ 
+		TableShort t = new TableShort(); 
+		t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		t.setSize(800, 400); 
+		t.setTitle("My Table"); 
+		t.setVisible(true); 
+	} 
+} 
