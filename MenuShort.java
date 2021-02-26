97b2aebc7dde954eb87e837f3ccc728582aaedbf
KAARTHIKEYAPAVANAKUMAAR <54408283+KAARTHIKEYAPAVANAKUMAAR@users.noreply.github.com>
2021-02-26 20:54:04 +0530

    Create MenuShort.java

---
+++ MenuShort.java
@@ -0,0 +1,38 @@
+package javalab;
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*;
+public class MenuShort extends JFrame implements ActionListener {
+ 	JMenuBar mb; 
+	JMenu draw; 
+	JMenuItem rect,line,oval; 
+	MenuShort() { 
+		mb=new JMenuBar(); 
+		draw=new JMenu("draw"); 
+		mb.add(draw); 
+		rect=new JMenuItem("rect");
+ 		rect.addActionListener(this); 
+		draw.add(rect); 
+		oval=new JMenuItem("oval"); 
+		oval.addActionListener(this); 
+		draw.add(oval); 
+		setJMenuBar(mb); 
+	} 
+	
+	public void actionPerformed(ActionEvent ae) { 
+		String str=ae.getActionCommand(); 
+		Graphics g=getGraphics(); 
+		if(str=="rect") 
+			g.drawRect(100,100,100,100); 
+		if(str=="oval") 												
+			g.drawOval(200,200,50,100); 
+	} 
+	
+	
+	public static void main(String args[]) { 									
+		MenuShort f=new MenuShort();									
+		f.setTitle("my frame"); 
+		f.setSize(500,500); 
+		f.setVisible(true); 							   		               	            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 	}
+ } 
